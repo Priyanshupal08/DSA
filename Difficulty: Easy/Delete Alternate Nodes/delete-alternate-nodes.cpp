be5e30a91318a1f2b2e1ce29e36af9399a1208e6
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-07 11:18:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Alternate Nodes/delete-alternate-nodes.cpp
@@ -0,0 +1,29 @@
+/*
+Structure of the node of the binary tree
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        int data = x;
+        next = NULL;
+    }
+};
+*/
+// Complete this function
+class Solution {
+  public:
+    void deleteAlt(struct Node *head) {
+        // Code here
+        Node* temp= head;
+        
+        while(temp && temp->next){
+            
+            temp->next=temp->next->next;
+            temp=temp->next;
+            
+        }
+        
+    }
+};
