b23931246ce549e2213bebe081b1b4460df4599e
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-12 11:32:31 +0530

    Time: 0 ms (100%), Space: 9 MB (54.89%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n= s.size();
+        int x=0;
+        
+        
+        
+        stack<char> st;
+        
+        string ans="";
+        
+        for(int i=0; i<n; i++){
+            
+            if(s[i]=='('){
+                
+                if(x==0){
+                    st.push(s[i]);
+                    x++;
+                    continue;
+                }
+                
+                st.push(s[i]);
+                ans+=s[i];
+                x++;
+            }
+            
+            else{
+                
+                if(x==1){
+                    st.pop();
+                    x--;
+                    continue;
+                }
+                
+                st.pop();
+                ans+=s[i];
+                
+                x--;
+                
+            }
+            
+        }
+        
+        return ans;
+    }
+};
