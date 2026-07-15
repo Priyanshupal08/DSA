54b95c669b5d73f8de803740f42d4b7158868e08
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-15 22:34:15 +0530

    Time: 0 ms (100%), Space: 15.5 MB (96.7%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -7,45 +7,17 @@ public:
         int a=0, b=0;
 
         for(int i=1; i<n-1; i++){
-
-            if(colors[i]=='A' && colors[i+1]=='A'){
-                if(colors[i-1]=='A'){
+            if(colors[i]==colors[i+1] && colors[i]==colors[i-1]){
+                if(colors[i]=='A'){
                     a++;
                 }
-            }
-
-            else if(colors[i]=='B' && colors[i+1]=='B'){
-                if(colors[i-1]=='B'){
+                else{
                     b++;
                 }
             }
         }
 
-        cout<<a<<" "<<b;
+        return a>b;
 
-        bool chance=1;
-
-        while(a>0 && b>0){
-
-            if(chance==1){
-                a--;
-                chance=0;
-            }
-
-            if(chance==0){
-                b--;
-                chance=1;
-            }
-        }
-
-        if(a==0){
-            return false;
-        }
-
-        else{
-            return true;
-        }
-
-        return 0;
     }
 };
