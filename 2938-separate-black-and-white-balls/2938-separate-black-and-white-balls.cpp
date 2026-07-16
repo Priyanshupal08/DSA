a73d0ee4f6739d65a7452803a371fa857c61b860
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-16 11:02:05 +0530

    Time: 6 ms (56.12%), Space: 17.3 MB (8.45%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        
+        int n=s.size();
+
+        long long count=0;
+        int i=0, j=n-1;
+
+        while(i<j){
+
+            if(s[i]=='1' && s[j]=='0'){
+                swap(s[i],s[j]);
+                count+= (j-i);
+            }
+
+            else if(s[i]=='0'){
+                i++;
+            }
+            else if(s[j]=='1'){
+                j--;
+            }
+
+        }
+
+        return count;
+
+
+
+    }
+};
