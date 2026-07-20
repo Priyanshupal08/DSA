e4a89b829942295525ceb32e0ab19f7ab02a74d2
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-20 12:01:51 +0530

    Time: 11 ms (25.11%), Space: 19 MB (55.21%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -4,16 +4,14 @@ public:
         
         int n= s.size();
 
-        int count= 0;
-
         for(int i=0; i<n; i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                count++;
+                return true;
             }
         }
 
 
-        return (count==0)? false: true;
+        return false;
 
 
     }
