e32c41c0d9dc32e6c7dbb25789500d9723cff9f1
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-20 10:31:44 +0530

    Time: 0 ms (100%), Space: 180.1 MB (1.08%) - LeetHub

---
+++ 3828-final-element-after-subarray-deletions/3828-final-element-after-subarray-deletions.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int finalElement(vector<int>& nums) {
+        
+        int n= nums.size();
+        if(nums[0]>nums[n-1]){
+            return nums[0];
+        }
+        else{
+            return nums[n-1];
+        }
+
+    }
+};
