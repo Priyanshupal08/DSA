5f1f81cc9b86b495f52edaa762c774e0dd242d21
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-07-27 09:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if there is a direct edge between two vertices/check-if-there-is-a-direct-edge-between-two-vertices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool checkEdge(vector<vector<int>>& adj, int u, int v) {
+        // code here
+        
+        for(int i=0; i<adj[u].size(); i++){
+            if(adj[u][i]==v){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+};
