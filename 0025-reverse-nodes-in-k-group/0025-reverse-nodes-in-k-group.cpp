9bfefa9abc11343d91245c5006cc1446426caacc
Priyanshupal08 <priyanshupal081206@gmail.com>
2026-08-27 00:34:15 +0530

    Time: 2 ms (3.19%), Space: 16.5 MB (39.47%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,95 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* rev(ListNode* head){
+
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* prev= head;
+        ListNode* curr= head->next;
+        ListNode* nxt= curr->next;
+
+        prev->next=NULL;
+        while(curr->next!=NULL){
+
+            curr->next= prev;
+            prev= curr;
+            curr=nxt;
+            nxt= curr->next;
+        }
+
+        curr->next= prev;
+        return curr;
+    }
+
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        
+        ListNode* temp1 = head;
+        ListNode* temp2 = head;
+        ListNode* ans = NULL;
+        ListNode* t;
+
+        if(k==1){
+            return head;
+        }
+
+        while(temp2){
+
+            // temp2= temp1;
+            int i=k;
+            while(i>1){
+                temp2=temp2->next;
+
+                if(temp2==NULL){
+                    while(t->next!=NULL){
+                        t=t->next;
+                    }
+                    while(temp1){
+                        t->next= temp1;
+                        t= t->next;
+                        temp1= temp1->next;
+                    }
+                    return ans;
+                }
+
+                i--;
+            }
+
+            // if(temp2->next!=NULL){
+            ListNode* n= temp2->next;
+            // }
+            temp2->next= NULL;
+            ListNode* r= rev(temp1);
+            if(ans==NULL){
+                ans= r;
+                t= ans;
+            }
+            else{
+                while(t->next!=NULL){
+                    t=t->next;
+                }
+
+                t->next=r;
+            }
+            temp1= n;
+            temp2= n;
+            
+        }
+
+        return ans;
+
+    }
+};
